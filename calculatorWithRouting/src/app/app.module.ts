b28bbfe14eb7d1cf7d2e653553b2cf80aac836b8
Miguel <2029ad@gmail.com>
2019-12-06 19:39:59 +0100

    I add the inputs to capture the two values and add some style

--- calculatorWithRouting/src/app/app.module.ts
+++ calculatorWithRouting/src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { AddComponent } from './components/add/add.component';
-import { SubtractComponent } from './components/subtract/subtract.component';
-import { MultiplyComponent } from './components/multiply/multiply.component';
-import { DivideComponent } from './components/divide/divide.component';
-import { HomeComponent } from './pages/home/home.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { AddComponent } from "./components/add/add.component";
+import { SubtractComponent } from "./components/subtract/subtract.component";
+import { MultiplyComponent } from "./components/multiply/multiply.component";
+import { DivideComponent } from "./components/divide/divide.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @NgModule({
   declarations: [
@@ -21,12 +21,8 @@ import { HomeComponent } from './pages/home/home.component';
     DivideComponent,
     HomeComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- calculatorWithRouting/src/app/pages/home/home.component.ts
+++ calculatorWithRouting/src/app/pages/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../../services/data.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
+  public number1: number;
+  public number2: number;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public dataService: DataService) {
+    this.number1 = dataService.number1;
+    this.number2 = dataService.number2;
   }
 
+  ngOnInit() {}
+
+  add() {
+    this.dataService.number1 = this.number1;
+    this.dataService.number2 = this.number2;
+  }
 }

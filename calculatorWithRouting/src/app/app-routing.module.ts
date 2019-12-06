78ab832f7001af2429658ce996b677a9610771d9
Miguel <2029ad@gmail.com>
2019-12-06 19:16:39 +0100

    navigation bar and routes

--- calculatorWithRouting/src/app/app-routing.module.ts
+++ calculatorWithRouting/src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
+//Import components
+import { HomeComponent } from "./pages/home/home.component";
+import { AddComponent } from "./components/add/add.component";
+import { SubtractComponent } from "./components/subtract/subtract.component";
+import { MultiplyComponent } from "./components/multiply/multiply.component";
+import { DivideComponent } from "./components/divide/divide.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "home", component: HomeComponent },
+  { path: "add", component: AddComponent },
+  { path: "subtract", component: SubtractComponent },
+  { path: "multiply", component: MultiplyComponent },
+  { path: "divide", component: DivideComponent },
+  { path: "**", redirectTo: "" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

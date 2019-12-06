28b52e54008057c4d32ccc09b68603ed806e1d03
Miguel <2029ad@gmail.com>
2019-12-06 20:05:40 +0100

    I show the result of the operation passing the values of the inputs using a service

--- calculatorWithRouting/src/app/components/add/add.component.ts
+++ calculatorWithRouting/src/app/components/add/add.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../../services/data.service";
 
 @Component({
-  selector: 'app-add',
-  templateUrl: './add.component.html',
-  styleUrls: ['./add.component.scss']
+  selector: "app-add",
+  templateUrl: "./add.component.html",
+  styleUrls: ["./add.component.scss"]
 })
 export class AddComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  public total: number;
+  constructor(public dataService: DataService) {
+    this.total = dataService.number1 + dataService.number2;
   }
 
+  ngOnInit() {}
 }

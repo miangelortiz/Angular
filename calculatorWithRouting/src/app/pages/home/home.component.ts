d177723c70a87e8d15b772457331652e829d70ad
Miguel <2029ad@gmail.com>
2019-12-09 10:50:06 +0100

    show results on the home page

--- calculatorWithRouting/src/app/pages/home/home.component.ts
+++ calculatorWithRouting/src/app/pages/home/home.component.ts
@@ -9,6 +9,10 @@ import { DataService } from "../../services/data.service";
 export class HomeComponent implements OnInit {
   public number1: number;
   public number2: number;
+  public totalAdd: number = 0;
+  public totalSubtract: number = 0;
+  public totalMultiply: number = 0;
+  public totalDivide: number = 0;
 
   constructor(public dataService: DataService) {
     this.number1 = dataService.number1;
@@ -17,7 +21,16 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {}
 
-  add() {
+  changeInput() {
+    if (this.number1 == null) {
+      this.number1 = 0;
+    } else if (this.number2 == null) {
+      this.number2 = 0;
+    }
+    this.totalAdd = this.number1 + this.number2;
+    this.totalSubtract = this.number1 - this.number2;
+    this.totalMultiply = this.number1 * this.number2;
+    this.totalDivide = this.number1 / this.number2;
     this.dataService.number1 = this.number1;
     this.dataService.number2 = this.number2;
   }
